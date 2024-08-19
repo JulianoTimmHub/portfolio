daa7c5f18c600ee112b662c0afb986b322c5af9a
JulianoTimmHub <julianohenriquetimm@gmail.com>
2024-08-19 13:49:33 -0300

    add recent projects

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,9 @@ export const Layout = ({ children }: any) => {
         <div className="container mx-auto">
             <header>
                 <nav className="w-full flex items-center justify-center gap-8">
-                    <Link className="hover:bg-cyan-300" href="/">Sobre mim</Link>
-                    <Link href="/">Contatos</Link>
-                    <Link href="/">Projetos</Link>
+                    <Link className="hover:bg-gray-500 rounded-full p-2" href="/">Sobre mim</Link>
+                    <Link className="hover:bg-gray-500 rounded-full p-2" href="/">Contatos</Link>
+                    <Link className="hover:bg-gray-500 rounded-full p-2" href="/">Projetos</Link>
                 </nav>
             </header>
             {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="mt-10">
-      <div className="w-full flex items-center justify-center">
+      <section className="w-full flex items-center justify-center">
         <div className="w-6/12 flex items-center justify-end">
           <div className="flex flex-col">
             <h1 className="text-6xl m-2">Olá, me chamo <strong>Juliano</strong></h1>
@@ -12,10 +12,40 @@ export default function Home() {
         </div>
         <div className="w-6/12 flex items-center justify-center">
           <div className="border-4 rounded-full p-3 border-blue-900">
-            <Image className="rounded-full" width={300} height={200} src="/profile.png" />
+            <Image className="rounded-full" alt="" width={300} height={200} src="/profile.png" />
           </div>
         </div>
-      </div>
+      </section>
+      <h1 className="text-3xl m-2 text-left">Projetos recentes</h1>
+      <section className="w-full flex items-center justify-evenly flex-wrap">
+        <div className="max-w-sm rounded overflow-hidden">
+          <Image className="w-full" width={300} height={200} src="/profile.png" alt="Sunset in the mountains" />
+          <div className="px-6 py-4 border-2 border-blue-900">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <Image className="w-full" width={300} height={200} src="/profile.png" alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <Image className="w-full" width={300} height={200} src="/profile.png" alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
