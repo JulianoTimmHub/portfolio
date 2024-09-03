2c9e42683f1e830444df8f99afcc74c55a96e8eb
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2024-09-03 19:16:22 -0300

    testing next ui components functionality in production

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/Tabs.js",
-    "./node_modules/@nextui-org/theme/dist/components/Tab.js",
-    "./node_modules/@nextui-org/theme/dist/components/Card.js",
-    "./node_modules/@nextui-org/theme/dist/components/CardBody.js",
-    './node_modules/@nextui-org/theme/dist/components/(Tabs|Tab|Card|CardBody).js'
+    "./node_modules/@nextui-org/theme/dist/components/*",
+    './node_modules/@nextui-org/theme/dist/components/(*).js'
   ],
   darkMode: "class",
   theme: {
